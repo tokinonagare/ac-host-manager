4108bf08fc1f709ee97680fc97ceb5545c61f712
markrookie <goubowenxxb@gmail.com>
2021-03-02 11:30:32 +0800

    增加一个sortByAccessSpeed接口

--- ACHostManager.js
+++ ACHostManager.js
@@ -27,22 +27,26 @@ export default class ACHostManager {
             if (fastestResult && fastestResult.available) {
                 return fastestResult;
             }
-            // 如果直接请求最快Host失败，再测速所有Host，返回最快的
-            const results = await this.speedTestModel.testAll(this._hosts);
-            const availableResults = results.filter(result => result.available);
-            if (availableResults.length === 0) {
-                return undefined;
-            }
-            const sortedResult = availableResults.sort((result1, result2) => {
-                // 按延迟从低到高排序
-                return result1.delay - result2.delay;
-            });
+            // 如果直接请求最快Host失败，把所有Host按访问速度排序
+            const sortedResults = await this.sortByAccessSpeed();
             // 返回第一个即是最快的
-            return sortedResult[0];
+            return sortedResults[0];
         } catch (error) {
             console.log(`----------------------------------- error.message :: ${error.message} \n\n`);
             throw error;
         }
+    }
+
+    async sortByAccessSpeed() {
+        const results = await this.speedTestModel.testAll(this._hosts);
+        const availableResults = results.filter(result => result.available);
+        if (availableResults.length === 0) {
+            return undefined;
+        }
+        return availableResults.sort((result1, result2) => {
+            // 按延迟从低到高排序
+            return result1.delay - result2.delay;
+        });
     }
 
     _random = () => Math.random() > 0.5;
